ee4de839d5b950e4dde06d5885d88a464a7d656e
Jorge Garcia de Bustos <j.garciadebustos@godeltech.com>
2018-07-03 17:00:18 +0100

    Fixes to make app.py run in the context of uWSGI

--- ml_rest_api/app.py
+++ ml_rest_api/app.py
@@ -11,11 +11,6 @@ import ml_rest_api.api.health.readiness
 import ml_rest_api.api.model.predict
 from ml_rest_api.api.restplus import api
 
-app = Flask(__name__)
-logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
-logging.config.fileConfig(logging_conf_path)
-log = logging.getLogger(__name__)
-
 def configure_app(flask_app):
     settings_to_apply = [
         #'FLASK_SERVER_NAME',
@@ -36,9 +31,14 @@ def initialize_app(flask_app):
     trained_model_wrapper.init()
 
 def main():
-    initialize_app(app)
     log.info('***** Starting development server at http://{}/api/ *****'.format(get_value('FLASK_SERVER_NAME')))
     app.run(debug=get_value('FLASK_DEBUG'), port=get_value('FLASK_PORT'), host=get_value('FLASK_HOST'))
+
+app = Flask(__name__)
+logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
+logging.config.fileConfig(logging_conf_path)
+log = logging.getLogger(__name__)
+initialize_app(app)
 
 if __name__ == "__main__":
     main()
